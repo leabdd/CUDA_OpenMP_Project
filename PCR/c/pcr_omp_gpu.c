fbfc1dbbbb99b383739b80f1eba92eabb860cb22
Lea Budde <lea.budde@uni-potsdam.de>
2026-03-01 16:05:51 +0100

    Separating Function for OMP GPU Version

--- PCR/c/pcr_omp_gpu.c
+++ PCR/c/pcr_omp_gpu.c
@@ -11,11 +11,46 @@
 
 #define EPSILON 1e-30
 
-// when including this with pragma omp declare target this function makes the code much slower, so we inline it and duplicate the logic in the kernel instead
-/* static inline float compute_decoupling_coeffs(float decoupling_value,
+#pragma omp declare target
+static inline float compute_decoupling_coeffs(float decoupling_value,
                                               float into_value) {
-  return -into_value / (decoupling_value == 0 ? EPSILON : decoupling_value);
-} */
+  // Reciprocal and multiply instead of division for better performance
+  const float inv = 1.0f / (decoupling_value == 0 ? EPSILON : decoupling_value);
+  return -into_value * inv;
+}
+#pragma omp end declare target
+
+// Compiler says this directive is ignored, because the function contains a directive, but it has impact on performance for some reason.
+#pragma omp declare target
+static inline void update_step_kernel(size_t n, size_t stride,
+                              float *restrict a_src, float *restrict b_src,
+                              float *restrict c_src, float *restrict d_src,
+                              float *restrict a_dst, float *restrict b_dst,
+                              float *restrict c_dst, float *restrict d_dst) {
+  #pragma omp target teams distribute parallel for map(present: a_src[0:n], b_src[0:n], c_src[0:n], d_src[0:n], \
+                        a_dst[0:n], b_dst[0:n], c_dst[0:n], d_dst[0:n])
+  for (int i = 0; i < (int)n; i++) {
+    int iRight = i + (int)stride;
+    int iLeft = i - (int)stride;
+
+    const float alpha = compute_decoupling_coeffs(iLeft < 0 ? 1.f : b_src[iLeft], a_src[i]);
+    const float gamma = compute_decoupling_coeffs(iRight < (int)n ? b_src[iRight] : 1.f, c_src[i]);
+
+    const float sa_iLeft = iLeft < 0 ? 0.0f : a_src[iLeft];
+    const float sc_iLeft = iLeft < 0 ? 0.0f : c_src[iLeft];
+    const float sd_iLeft = iLeft < 0 ? 0.0f : d_src[iLeft];
+
+    const float sa_iRight = iRight >= (int)n ? 0.0f : a_src[iRight];
+    const float sc_iRight = iRight >= (int)n ? 0.0f : c_src[iRight];
+    const float sd_iRight = iRight >= (int)n ? 0.0f : d_src[iRight];
+
+    a_dst[i] = alpha * sa_iLeft;
+    c_dst[i] = gamma * sc_iRight;
+    b_dst[i] = b_src[i] + alpha * sc_iLeft + gamma * sa_iRight;
+    d_dst[i] = d_src[i] + alpha * sd_iLeft + gamma * sd_iRight;
+  }
+}
+#pragma omp end declare target
 
 int pcr(triSLE_t *sle, timer *start, timer *end) {
   if (sle == NULL) {
@@ -54,8 +89,7 @@ int pcr(triSLE_t *sle, timer *start, timer *end) {
 
   TIME_GET(*start);
   
-  // Map data pointers to GPU for duration of computation
-  // Create data region so memory stays on the GPU between iterations
+  // Copy data to GPU and allocate swap on GPU
   #pragma omp target data map(to: a_data[0:n], b_data[0:n], c_data[0:n], d_data[0:n]) \
                         map(alloc: a_swap[0:n], b_swap[0:n], c_swap[0:n], d_swap[0:n]) \
                         map(from: x_data[0:n])
@@ -73,44 +107,15 @@ int pcr(triSLE_t *sle, timer *start, timer *end) {
         a_dst = a_data; b_dst = b_data; c_dst = c_data; d_dst = d_data;
       }
       
-      // Update Step Kernel runs on GPU
-      #pragma omp target teams distribute parallel for map(present: a_src[0:n], b_src[0:n], \
-                        c_src[0:n], d_src[0:n], a_dst[0:n], b_dst[0:n], c_dst[0:n], d_dst[0:n])
-      for (int i = 0; i < (int)n; i++) {
-        int iRight = i + (int)stride;
-        int iLeft = i - (int)stride;
-
-        // Directly inlining the decoupling coefficient made the code faster.
-        // Even more benefit was achieved by using the reciprocal and then multiplying instead.
-        float decoupling_value = iLeft < 0 ? 1.f : b_src[iLeft];
-        float inv = 1.0f / (decoupling_value == 0 ? EPSILON : decoupling_value);
-        const float alpha = -a_src[i] * inv;
-
-        decoupling_value = iRight < (int)n ? b_src[iRight] : 1.f;
-        inv = 1.0f / (decoupling_value == 0 ? EPSILON : decoupling_value);
-        const float gamma = -c_src[i] * inv;
-
-        const float sa_iLeft = iLeft < 0 ? 0.0f : a_src[iLeft];
-        const float sc_iLeft = iLeft < 0 ? 0.0f : c_src[iLeft];
-        const float sd_iLeft = iLeft < 0 ? 0.0f : d_src[iLeft];
-
-        const float sa_iRight = iRight >= (int)n ? 0.0f : a_src[iRight];
-        const float sc_iRight = iRight >= (int)n ? 0.0f : c_src[iRight];
-        const float sd_iRight = iRight >= (int)n ? 0.0f : d_src[iRight];
-
-        a_dst[i] = alpha * sa_iLeft;
-        c_dst[i] = gamma * sc_iRight;
-        b_dst[i] = b_src[i] + alpha * sc_iLeft + gamma * sa_iRight;
-        d_dst[i] = d_src[i] + alpha * sd_iLeft + gamma * sd_iRight;
-      }
+      update_step_kernel(n, stride, a_src, b_src, c_src, d_src, a_dst, b_dst, c_dst, d_dst);
     }
 
     // Added SIMD directive, but seen no performance improvement
     // Back substitution kernel
-    // Reciprocal and multiply instead of division for better performance
     #pragma omp target teams distribute parallel for simd map(present: b_data[0:n], \
                         d_data[0:n], x_data[0:n])
     for (size_t i = 0; i < n; i++) {
+    // Reciprocal and multiply instead of division for better performance
       const float inv = 1.0f / b_data[i];
       x_data[i] = d_data[i] * inv;
     }
